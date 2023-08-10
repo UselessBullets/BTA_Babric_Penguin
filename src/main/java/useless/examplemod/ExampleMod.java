3a9ec916629babdae3527abda10c78a769f08281
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-08-10 01:36:29 -0500

    Initial commit

---
+++ src/main/java/useless/examplemod/ExampleMod.java
@@ -0,0 +1,22 @@
+package useless.examplemod;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import useless.config.ModMenuConfig;
+import useless.config.ModMenuConfigManager;
+
+public class ExampleMod implements ModInitializer {
+    public static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).setPrettyPrinting().create();
+    public static final String MOD_ID = "examplemod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        ModMenuConfigManager.initializeConfig();
+        LOGGER.info("ExampleMod initialized.");
+    }
+}
